5107fd53651139ec17b6df1d9992a7adf61f4f15
Ioan Vlad <ivlad@ivlad.info>
2016-01-31 21:42:09 -0600

    Downloaded data

--- GettingCleaningDataProj.R
+++ GettingCleaningDataProj.R
@@ -1,2 +1,9 @@
 # Getting and Cleaning Data Course Project
 # Copyright Ioan Vlad 2016
+
+zip_URL <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
+zip_local <- 'raw.zip'
+
+download.file(zip_URL, zip_local)
+
+unzip(zip_local)
